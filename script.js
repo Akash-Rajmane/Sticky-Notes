59444db83bc6fb6df3d69bf73281720f00826e9c
Akash <akashrajmane23@gmail.com>
2023-11-07 00:44:57 +0530

    edit, del functions

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ window.onload = document.querySelector("#user_input").select();
 
 const notes = JSON.parse(localStorage.getItem('notes'))
 
-if(notes) {
+
+window.addEventListener("DOMContentLoaded",()=>{
+  if(notes) {
     notes.forEach(note => createStickyNote(note))
-}
+  }
+})
+
 
 document.querySelector("#add_note").addEventListener("click", () => {
   document.querySelector("#modal").style.display = "block";
@@ -23,43 +27,60 @@ document.querySelector("#user_input").addEventListener('keydown', (event) => {
   if(event.key === 'Enter'){
     const text = document.querySelector("#user_input");
     createStickyNote(text.value);
+    text.value = "";
   }
 });
 
-createStickyNote = (text) => {
+function createStickyNote(text){
   let note = document.createElement("div");
   let details = document.createElement("div");
   let noteText = document.createElement("h1");
+  let delBtn = document.createElement("button");
+  let editBtn = document.createElement("button");
 
+  delBtn.textContent = "X";
+  editBtn.textContent = "edit";
+
+
+  note.appendChild(delBtn);
+  note.appendChild(editBtn);
   note.className = "note";
   details.className = "details";
   noteText.textContent = text;
-
+  noteText.className = "text";
   details.appendChild(noteText);
   note.appendChild(details);
     
-   updateLS();
+ 
 
   if(index > random_colors.length - 1)
     index = 0;
 
   note.setAttribute("style", `margin:${random_margin[Math.floor(Math.random() * random_margin.length)]}; background-color:${random_colors[index++]}; transform:${random_degree[Math.floor(Math.random() * random_degree.length)]}`);
 
-  note.addEventListener("dblclick", () => {
-    note.remove();
-  })
-
   document.querySelector("#all_notes").appendChild(note);
+  
+  delBtn.addEventListener("click",deleteNote)
+  function deleteNote(event){
+      const newNotes = notes.filter((el) => el !== text); // Filter out the clicked note
+      localStorage.setItem('notes', JSON.stringify(newNotes));
+    // Remove the note from the DOM
+      event.target.parentNode.remove();
+  }
+
+  editBtn.addEventListener("click",(event)=>{
+    document.querySelector("#modal").style.display = "block";
+    const noteText = document.querySelector("#user_input");
+    noteText.value = text;
+    deleteNote(event);
+  })
+  updateLS();
 }
 
 function updateLS() {
-    const notesText = document.querySelectorAll('textarea')
-
-    const notes = []
-
-    notesText.forEach(note => notes.push(note.value))
-
-    localStorage.setItem('notes', JSON.stringify(notes))
+  const notesText = document.querySelectorAll('.text');
+  const notesArr = Array.from(notesText).map((note) => note.textContent);
+  localStorage.setItem('notes', JSON.stringify(notesArr));
 }
 
 
